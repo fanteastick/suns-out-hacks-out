4cd6cccf2349e6dd6a342ab38c79b80ea11659ef
Eugene Shao <31578309+ptes77@users.noreply.github.com>
2020-08-29 21:06:39 -0400

    Update face_filter.py
    refactor pt 2/infinity

--- face_filter.py
+++ face_filter.py
@@ -1,14 +1,15 @@
 from emotions import FilterEmotions, emotion_mapping
 from filter_pos import *
 
-single_face_image_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfkrYgTxf2uCXFHxi7t2QUIaefqfcpsm-jGg&usqp=CAU'
-ffilt = faceFilter()
-# img = ffilt.addFilterURL(single_face_image_url, 0, 0)
-path_pics = pathlib.Path().cwd() / "pictures"
-list_of_pics = glob.glob(os.path.join(path_pics, '*'))
-image_list = []
-for pic in list_of_pics:
-    image_list.append(pic)
-img = cv2.imread(image_list[2])
-img = ffilt.addFilter(img, 0, 0)
-img.show()
+if __name__ == '__main__':
+    single_face_image_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfkrYgTxf2uCXFHxi7t2QUIaefqfcpsm-jGg&usqp=CAU'
+    ffilt = faceFilter()
+    # img = ffilt.addFilterURL(single_face_image_url, 0, 0)
+    path_pics = pathlib.Path().cwd() / "pictures"
+    list_of_pics = glob.glob(os.path.join(path_pics, '*'))
+    image_list = []
+    for pic in list_of_pics:
+        image_list.append(pic)
+    img = cv2.imread(image_list[2])
+    img = ffilt.addFilter(img, 0, 0)
+    img.show()
